9077d0a7e3f43ce703dc784bf756e8738697bcfd
unknown <filjoabraham@gmail.com>
2020-10-01 20:23:11 -0500

    Create register screen

---
+++ app/src/main/java/com/proshiftteam/proshift/RegisterActivity.kt
@@ -0,0 +1,15 @@
+package com.proshiftteam.proshift
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        val btnSubmit = findViewById<Button>(R.id.btnRegisterSubmit)
+
+    }
+}
