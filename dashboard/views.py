27033d273d07120c29632ab917c0419caa03dc1f
unknown <filjoabraham@gmail.com>
2020-10-20 20:52:36 -0500

    Create login and registration page

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
+from django.contrib import messages
+
+from .forms import RegistrationForm
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or password is incorrect')
+
+    context = {}
+    return render(request, 'login.html', context)
+
+def register_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('login')
+
+    context = {'form': form}
+    return render(request, 'register.html', context)
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
 def home_view(request):
     return render(request, 'home.html')
