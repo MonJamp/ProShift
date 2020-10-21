f171bbe4afb3d555bfeca9b5baabd5700e01b321
unknown <filjoabraham@gmail.com>
2020-10-20 23:28:34 -0500

    API: fix api register url

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register', views.UserCreate.as_view(), name='register')
+    path('register', views.UserCreate.as_view(), name='api_register')
 ]
