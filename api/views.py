3c189585fe6e62ccb47353c282a3b928d1046e65
unknown <filjoabraham@gmail.com>
2020-10-20 22:43:11 -0500

    API: use authorization token

--- api/views.py
+++ api/views.py
@@ -1,3 +1,5 @@
+from rest_framework.authtoken.models import Token
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -12,6 +14,9 @@ class UserCreate(APIView):
         if serializer.is_valid():
             user = serializer.save()
             if user:
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
+                token = Token.objects.create(user=user)
+                json = serializer.data
+                json['token'] = token.key
+                return Response(json, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
