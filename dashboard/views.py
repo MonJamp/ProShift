4011575d7c27af4320e4320537198e653c286bb7
unknown <filjoabraham@gmail.com>
2020-10-22 20:58:29 -0500

    prefix url patterns to avoid naming conflicts

--- dashboard/views.py
+++ dashboard/views.py
@@ -10,7 +10,7 @@ from .forms import RegistrationForm
 
 def login_view(request):
     if request.user.is_authenticated:
-        return redirect('home')
+        return redirect('d-home')
 
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -20,7 +20,7 @@ def login_view(request):
 
         if user is not None:
             login(request, user)
-            return redirect('home')
+            return redirect('d-home')
         else:
             messages.info(request, 'Username or password is incorrect')
 
@@ -29,7 +29,7 @@ def login_view(request):
 
 def register_view(request):
     if request.user.is_authenticated:
-        return redirect('home')
+        return redirect('d-home')
     
     form = RegistrationForm()
     if request.method == 'POST':
@@ -38,15 +38,15 @@ def register_view(request):
             form.save()
             user = form.cleaned_data.get('username')
             messages.success(request, 'Account was created for ' + user)
-            return redirect('login')
+            return redirect('d-login')
 
     context = {'form': form}
     return render(request, 'register.html', context)
 
 def logout_view(request):
     logout(request)
-    return redirect('login')
+    return redirect('d-login')
 
-@login_required(login_url='login')
+@login_required(login_url='d-login')
 def home_view(request):
     return render(request, 'home.html')
