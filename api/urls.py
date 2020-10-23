b17f9ea7bb85e5d4c9fd21eda5c919adf45e8b9c
unknown <filjoabraham@gmail.com>
2020-10-22 21:41:51 -0500

    Use djoser for registration/login

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
-    path('register', views.UserCreate.as_view(), name='api_register')
+    path('register', views.UserCreate.as_view(), name='api_register'),
+    path('', include('djoser.urls')),
+    path('', include('djoser.urls.authtoken')),
 ]
