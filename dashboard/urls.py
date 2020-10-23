0b44c520b4da3c114226351f75cfd80b73131cd4
unknown <filjoabraham@gmail.com>
2020-10-22 20:58:29 -0500

    prefix url patterns to avoid naming conflicts

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name='home'),
+    path('', views.home_view, name='d-home'),
 
-    path('login/', views.login_view, name='login'),
-    path('register/', views.register_view, name='register'),
-    path('logout/', views.logout_view, name='logout')
+    path('login/', views.login_view, name='d-login'),
+    path('register/', views.register_view, name='d-register'),
+    path('logout/', views.logout_view, name='d-logout')
 ]
