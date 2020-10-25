da0e8a203ddbfecf6a11055838dab27f5619d540
unknown <filjoabraham@gmail.com>
2020-10-25 15:47:08 -0500

    api: create api interface for getting assigned shifts

--- api/views.py
+++ api/views.py
@@ -1,12 +1,17 @@
 from rest_framework.authtoken.models import Token
 
 from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 
 from django.contrib.auth.models import User
+from dashboard.models import Shift
 
-from .serializers import UserSerializer
+from .serializers import *
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.response import Response
+from django.http import HttpResponse, JsonResponse
 
 class UserCreate(APIView):
     def post(self, request, format='json'):
@@ -20,3 +25,10 @@ class UserCreate(APIView):
                 return Response(json, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def GetAssignedShifts(request, *args, **kwargs):
+    shifts = Shift.objects.filter(employee=request.user)
+    serializer = ShiftSerializer(shifts, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
