4e740fc6feae1ceeb2ecb9e708ea7fdbeca76543
unknown <filjoabraham@gmail.com>
2020-10-25 15:47:08 -0500

    api: create api interface for getting assigned shifts

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,7 @@ from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
-from dashboard.models import User
+from dashboard.models import User, Shift
 
 
 class UserCreateSerializer(UserCreateSerializer):
@@ -10,21 +10,7 @@ class UserCreateSerializer(UserCreateSerializer):
         model = User
         fields = ('id', 'username', 'email', 'password', 'first_name', 'last_name', 'phone', 'company_code')
 
-'''
-class UserSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
-    email = serializers.CharField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
-    first_name = serializers.CharField(required=True)
-    last_name = serializers.CharField(required=True)
-    password = serializers.CharField(required=True, min_length=8, write_only=True)
-
-    def create(self, validated_data):
-        user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
-        user.first_name = validated_data['first_name']
-        user.last_name = validated_data['last_name']
-        return user
-
+class ShiftSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'password', 'first_name', 'last_name', 'phone', 'company_code']
-'''
+        model = Shift
+        fields = ('employee', 'is_open', 'is_dropped', 'date', 'time_start', 'time_end')
--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('register', views.UserCreate.as_view(), name='api_register'),
+    path('get_shifts', views.GetAssignedShifts, name='api_shifts'),
     path('', include('djoser.urls')),
     path('', include('djoser.urls.authtoken')),
 ]
