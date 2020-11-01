804c11646beacdd4a3ef35e96458e31628944d82
Rupin Patel <70920375+rupinpatel@users.noreply.github.com>
2020-11-01 16:03:52 -0600

    Login Screen API POST
    Allows users to login,
    Created a new LoginObject which contains login information as well as variables for responses.
    Added the api post request for login in ApiCalls.kt
    Receives an auth_token in response which is stored on a variable named auth_token which can be used throughout the application.
    (Kept the old local users in the code for testing purposes)

--- app/src/main/java/com/proshiftteam/proshift/Activities/MainActivity.kt
+++ app/src/main/java/com/proshiftteam/proshift/Activities/MainActivity.kt
@@ -4,8 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import com.proshiftteam.proshift.DataFiles.LoginObject
+import com.proshiftteam.proshift.DataFiles.Registration
+import com.proshiftteam.proshift.Interfaces.ApiCalls
 import com.proshiftteam.proshift.R
 import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +28,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnLoginLogin.setOnClickListener {
-            val username: String = editTextLoginUsername.text.toString()
+            val emailAddress: String = editTextLoginUsername.text.toString()
             val password: String = editTextLoginPassword.text.toString()
 
-            if (username=="mlogin" && password=="password") {
+            val context = this
+            if (emailAddress=="mlogin@email.com" && password=="password") {
 
                 // Temporary Code for manager login for testing
                 val accessCode = 1
@@ -31,7 +41,7 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intentToHome)
 
 
-            } else if (username=="elogin" && password=="password") {
+            } else if (emailAddress=="elogin@email.com" && password=="password") {
 
                 // Temporary Code for employee login for testing
                 val accessCode = 0
@@ -40,6 +50,43 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intentToHome)
 
             } else {
+
+
+                val loginValuesObject = LoginObject(password, emailAddress)
+                val retrofitBuilder = Retrofit.Builder()
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl("http://proshiftonline.com/api/")
+                    .build()
+
+                val connectJsonApiCalls = retrofitBuilder.create(ApiCalls::class.java)
+
+                val callApiPost = connectJsonApiCalls.loginUser(loginValuesObject)
+
+                callApiPost.enqueue(object : Callback<LoginObject> {
+                    override fun onFailure(call: Call<LoginObject>, t: Throwable) {
+                        Toast.makeText(context, "Cannot connect!", Toast.LENGTH_SHORT).show()
+                    }
+                    override fun onResponse(
+                        call: Call<LoginObject>,
+                        response: Response<LoginObject>
+                    ) {
+                        if (response.code()==200) {
+                            val responseLoginObject = response.body()
+                            val accessCode = 1
+                            val tokenCode = responseLoginObject?.auth_token
+                            val intentToHome = Intent(context, HomeActivity::class.java)
+
+
+                            intentToHome.putExtra("accessCode", accessCode)
+                            intentToHome.putExtra("tokenCode", tokenCode)
+                            startActivity(intentToHome)
+
+                            Toast.makeText(context, "Welcome " + emailAddress + "! Authorization Token: " + tokenCode, Toast.LENGTH_SHORT).show() }
+                        else {
+                            Toast.makeText(context, "Error Logging in! Response Code: " + response.code(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                })
 
 
                 // Code for retrieving user information and connecting with appropriate homescreen
--- app/src/main/java/com/proshiftteam/proshift/Interfaces/ApiCalls.kt
+++ app/src/main/java/com/proshiftteam/proshift/Interfaces/ApiCalls.kt
@@ -1,6 +1,7 @@
 package com.proshiftteam.proshift.Interfaces
 
 
+import com.proshiftteam.proshift.DataFiles.LoginObject
 import com.proshiftteam.proshift.DataFiles.Registration
 import retrofit2.Call
 import retrofit2.http.Body
@@ -10,4 +11,7 @@ import retrofit2.http.POST
 interface ApiCalls {
     @POST("users/")
     fun registerUser(@Body registration: Registration): Call<Registration>
+
+    @POST("token/login")
+    fun loginUser(@Body loginObject: LoginObject): Call <LoginObject>
 }
