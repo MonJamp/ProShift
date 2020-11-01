57c5153ae025ccf910d4a3f2109d2bd053c82e6e
sorrick <63976854+sorrick@users.noreply.github.com>
2020-11-01 16:54:57 -0600

    Create Requested Time Off Model

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('register', views.UserCreate.as_view(), name='api_register'),
     path('get_shifts', views.GetAssignedShifts, name='api_shifts'),
+    path('get_requested_time_off', views.GetRequestedTimeOff, name='api_requested_time_off'),
     path('', include('djoser.urls')),
     path('', include('djoser.urls.authtoken')),
 ]
