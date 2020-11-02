c971cab634037f45ee5264fe3f05cd2d21739045
sorrick <63976854+sorrick@users.noreply.github.com>
2020-11-02 17:56:55 -0600

    Create Availability Model
    Create Availability Model

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -7,6 +7,12 @@ from .models import *
 class UserAdmin(admin.ModelAdmin):
     list_display = ('email', 'username', 'first_name', 'last_name', 'company_code', 'phone')
 
+
+class AvailabilityAdmin(admin.ModelAdmin):
+    list_display = ('employee' , 'company' , 'start_date', 'is_approved', 'is_current')
+
+
 admin.site.register(User, UserAdmin)
 admin.site.register(Shift)
 admin.site.register(RequestedTimeOff)
+admin.site.register(Availability, AvailabilityAdmin)
