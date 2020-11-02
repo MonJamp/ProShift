38605bef19e175c1c3f0a5f06db82a04986c895c
sorrick <63976854+sorrick@users.noreply.github.com>
2020-11-01 20:34:10 -0600

    Test more edge cases for user login

--- api/tests.py
+++ api/tests.py
@@ -68,7 +68,65 @@ class AccountsTest(APITestCase):
         }
 
         response = self.client.post(self.create_url, data, format='json')
+        # make sure that the request didn't go through(bad request status response)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        # make sure the user account wasn't added to the database
         self.assertEqual(User.objects.count(), 1)
         self.assertEqual(len(response.data['password']), 1)
 
+    def test_create_user_with_no_username(self):
+        data = {
+                'username': '',
+                'email': 'foobarbaz@example.com',
+                'first_name': 'foo',
+                'last_name': 'bar',
+                'password': 'somepassword'
+        }
+        response = self.client.post(self.create_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 1)
+
+    def test_create_user_with_no_email(self):
+        data = {
+                'username': 'foobar',
+                'email': '',
+                'first_name': 'foo',
+                'last_name': 'bar',
+                'password': 'somepassword'
+        }
+        response = self.client.post(self.create_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 1)
+    
+    def test_create_user_with_no_firstname(self):
+        data = {
+                'username': 'foobar',
+                'email': 'foobarbaz@example.com',
+                'first_name': '',
+                'last_name': 'bar',
+                'password': 'somepassword'
+        }
+        response = self.client.post(self.create_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 1)
+
+    def test_create_user_with_no_lastname(self):
+        data = {
+                'username': 'foobar',
+                'email': 'foobarbaz@example.com',
+                'first_name': 'foo',
+                'last_name': '',
+                'password': 'somepassword'
+        }
+        response = self.client.post(self.create_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 1)  
+
+    #def test_create_user_that_already_exists(self):
+        # we want to make sure that no 2 users have the same username
+    
+    #def test_create_user_with_nonunique_password(self):
+        # we want to make sure that each user has a unique password
+    
+    #def test_create_user_nonunique_usernames_and_emails(self):
+        # want to make sure that the username has entered a unique username and email
