2f1681fee8eb4331b4f488d894034253992b12ca
unknown <filjoabraham@gmail.com>
2020-11-02 21:45:19 -0600

    Show employee model under individual users in admin panel

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -4,12 +4,25 @@ from .models import *
 
 # Register your models here.
 
+class EmployeeInline(admin.StackedInline):
+    model = Employee
+    can_delete = False
+    verbose_name_plural = 'Employee'
+    fk_name = 'user'
+    extra = 0
+
 class UserAdmin(admin.ModelAdmin):
     list_display = ('email', 'username', 'first_name', 'last_name', 'company_code', 'phone')
 
 
 class AvailabilityAdmin(admin.ModelAdmin):
     list_display = ('employee' , 'company' , 'start_date', 'is_approved', 'is_current')
+    inlines = (EmployeeInline, )
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(UserAdmin, self).get_inline_instances(request, obj)
 
 class PositionAdmin(admin.ModelAdmin):
     list_display = ('name', 'company', 'is_manager')
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.base_user import BaseUserManager
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -88,5 +90,13 @@ class Position(models.Model):
 
 class Employee(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True)
-    position = models.ForeignKey(Position, on_delete=models.SET_NULL, null=True)
+    company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True, blank=True)
+    position = models.ForeignKey(Position, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.email
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Employee.objects.create(user=instance)
