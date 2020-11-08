c8c7444e8ad1f2c3a990f3661f74d4c5e2a0c935
unknown <filjoabraham@gmail.com>
2020-11-07 21:26:55 -0600

    api: Separate employee and manager API urls

--- api/urls.py
+++ api/urls.py
@@ -2,12 +2,22 @@ from django.urls import path, include
 
 from . import views
 
-urlpatterns = [
-    path('register', views.UserCreate.as_view(), name='api_register'),
+# Place employee related APIs here
+employee_urls = [
     path('get_shifts', views.GetAssignedShifts, name='api_shifts'),
     path('get_requested_time_off', views.GetRequestedTimeOff, name='api_requested_time_off'),
     path('get_availability', views.GetAvailability, name='api_get_availability'),
-    path('test_manager', views.TestManagerRole, name='api_test_manager'),
+]
+
+# Place manager related APIs here
+manager_urls = [
+    path('test', views.TestManagerRole, name='api_test_manager'),
+]
+
+urlpatterns = [
+    path('employee/', include(employee_urls)),
+    path('manager/', include(manager_urls)),
+    path('register', views.UserCreate.as_view(), name='api_register'),
     path('', include('djoser.urls')),
     path('', include('djoser.urls.authtoken')),
 ]
