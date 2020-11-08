888132aa6aa3a4fae48e1e14eaf0bb6dcb3771c7
unknown <filjoabraham@gmail.com>
2020-11-07 20:47:01 -0600

    Link models to company and employee role

--- dashboard/models.py
+++ dashboard/models.py
@@ -20,57 +20,6 @@ class User(AbstractUser):
     def __str__(self):
         return str(self.first_name) + " " + str(self.last_name)
 
-class Shift(models.Model):
-    employee = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    is_open = models.BooleanField(default=False)
-    is_dropped = models.BooleanField(default=False)
-    date = models.DateField()
-    time_start = models.TimeField()
-    time_end = models.TimeField()
-
-    REQUIRED_FIELDS = ['date', 'time_start', 'time_end']
-
-    def __str__(self):
-        return ( str(self.date) + " | " + str(self.time_start) + " - " + str(self.time_end) + " | " + self.employee.__str__())
-
-class RequestedTimeOff(models.Model):
-    company = models.CharField(null=True, blank=True, max_length=60)
-    employee = models.ForeignKey(User,on_delete=models.CASCADE, null=True, blank=True)
-    is_approved = models.BooleanField(default=False)
-    start_date = models.DateField()
-    end_date =  models.DateField()
-    time_start = models.TimeField()
-    time_end = models.TimeField()
-
-    REQUIRED_FIELDS = ['start_date', 'end_date', 'time_start', 'time_end']
-
-    def __str__(self):
-        return(str(self.start_date) + " - " + str(self.end_date) + " | " + str(self.time_start) + " - " + str(self.time_end) + " | " + self.employee.__str__())
-
-
-class Availability(models.Model):
-    company = models.CharField(null=True, blank=True, max_length=60)
-    employee = models.ForeignKey(User,on_delete=models.CASCADE, null=True, blank=True)
-    start_date = models.DateField()
-    is_approved = models.BooleanField(default=False)
-    is_current = models.BooleanField(default=False)
-    mon_earliest = models.TimeField()
-    mon_latest = models.TimeField()
-    tues_earliest = models.TimeField()
-    tues_latest = models.TimeField()
-    wed_earliest = models.TimeField()
-    wed_latest = models.TimeField()
-    thur_earliest = models.TimeField()
-    thur_latest = models.TimeField()
-    fri_earliest = models.TimeField()
-    fri_latest = models.TimeField()
-    sat_earliest = models.TimeField()
-    sat_latest = models.TimeField()
-    sun_earliest = models.TimeField()
-    sun_latest = models.TimeField()
-
-    def __str__(self):
-        return (str(self.employee) + " | " + str(self.start_date))
 class Company(models.Model):
     name = models.CharField(max_length=32)
 
@@ -102,7 +51,7 @@ class EmployeeRole(models.Model):
         blank=True) #models.ForeignKey(Position, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
-        return self.user.email
+        return str(self.user) + " | " + str(self.position)
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
@@ -117,3 +66,57 @@ def create_default_company_positions(sender, instance, created, **kwargs):
     if created:
         Position.objects.create(company=instance, name='Manager', is_manager=True)
         Position.objects.create(company=instance, name='Employee', is_manager=False)
+
+class Shift(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    employee = ChainedForeignKey(EmployeeRole, chained_field='company', chained_model_field='company')
+    is_open = models.BooleanField(default=False)
+    is_dropped = models.BooleanField(default=False)
+    date = models.DateField()
+    time_start = models.TimeField()
+    time_end = models.TimeField()
+
+    REQUIRED_FIELDS = ['date', 'time_start', 'time_end']
+
+    def __str__(self):
+        return ( str(self.date) + " | " + str(self.time_start) + " - " + str(self.time_end) + " | " + self.employee.__str__())
+
+class RequestedTimeOff(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    employee = ChainedForeignKey(EmployeeRole, chained_field='company', chained_model_field='company')
+    is_approved = models.BooleanField(default=False)
+    start_date = models.DateField()
+    end_date =  models.DateField()
+    time_start = models.TimeField()
+    time_end = models.TimeField()
+
+    REQUIRED_FIELDS = ['start_date', 'end_date', 'time_start', 'time_end']
+
+    def __str__(self):
+        return(str(self.start_date) + " - " + str(self.end_date) + " | " + str(self.time_start) + " - " + str(self.time_end) + " | " + self.employee.__str__())
+
+
+class Availability(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    employee = ChainedForeignKey(EmployeeRole, chained_field='company', chained_model_field='company')
+    #employee = models.ForeignKey(User,on_delete=models.CASCADE, null=True, blank=True)
+    start_date = models.DateField()
+    is_approved = models.BooleanField(default=False)
+    is_current = models.BooleanField(default=False)
+    mon_earliest = models.TimeField()
+    mon_latest = models.TimeField()
+    tues_earliest = models.TimeField()
+    tues_latest = models.TimeField()
+    wed_earliest = models.TimeField()
+    wed_latest = models.TimeField()
+    thur_earliest = models.TimeField()
+    thur_latest = models.TimeField()
+    fri_earliest = models.TimeField()
+    fri_latest = models.TimeField()
+    sat_earliest = models.TimeField()
+    sat_latest = models.TimeField()
+    sun_earliest = models.TimeField()
+    sun_latest = models.TimeField()
+
+    def __str__(self):
+        return (str(self.employee) + " | " + str(self.start_date))
