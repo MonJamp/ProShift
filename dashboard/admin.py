6cfbf990ee78c5a465e565a5fb935c357adca781
MonJamp <filjoabraham@gmail.com>
2020-11-12 21:49:42 -0600

    Add columns and show ids for every model

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -12,7 +12,7 @@ class EmployeeStatusInline(admin.StackedInline):
     extra = 0
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'username', 'first_name', 'last_name', 'company_code', 'phone')
+    list_display = ('email', 'username', 'first_name', 'last_name', 'company_code', 'phone', 'id')
     inlines = (EmployeeStatusInline, )
 
     def get_inline_instances(self, request, obj=None):
@@ -21,22 +21,31 @@ class UserAdmin(admin.ModelAdmin):
         return super(UserAdmin, self).get_inline_instances(request, obj)
 
 class AvailabilityAdmin(admin.ModelAdmin):
-    list_display = ('employee' , 'company' , 'start_date', 'is_approved', 'is_current')
+    list_display = ('employee' , 'company' , 'start_date', 'is_approved', 'is_current', 'id')
 
 class PositionAdmin(admin.ModelAdmin):
-    list_display = ('name', 'company', 'is_manager')
+    list_display = ('name', 'company', 'is_manager', 'id')
 
 class EmployeeRoleAdmin(admin.ModelAdmin):
-    list_display = ('user', 'company', 'position')
+    list_display = ('user', 'company', 'position', 'id')
 
 class ShiftRequestAdmin(admin.ModelAdmin):
-    list_display = ('employee', 'company', 'shift', 'is_approved')
+    list_display = ('employee', 'company', 'shift', 'is_approved', 'id')
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+
+class RequestedTimeOffAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'company', 'is_approved', 'start_date', 'end_date', 'time_start', 'time_end', 'id')
+
+class ShiftAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'company', 'is_open', 'is_dropped', 'date', 'time_start', 'time_end', 'id')
 
 admin.site.register(User, UserAdmin)
-admin.site.register(Shift)
-admin.site.register(Company)
+admin.site.register(Shift, ShiftAdmin)
+admin.site.register(Company, CompanyAdmin)
 admin.site.register(Position, PositionAdmin)
 admin.site.register(EmployeeRole, EmployeeRoleAdmin)
-admin.site.register(RequestedTimeOff)
+admin.site.register(RequestedTimeOff, RequestedTimeOffAdmin)
 admin.site.register(Availability, AvailabilityAdmin)
 admin.site.register(ShiftRequest, ShiftRequestAdmin)
