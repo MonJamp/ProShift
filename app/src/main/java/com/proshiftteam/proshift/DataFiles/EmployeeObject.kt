1930f2db5fd09ad7c9d310b70a259bcdca180f2c
MonJamp <filjoabraham@gmail.com>
2020-11-15 00:07:13 -0600

    Get list of employees and show in spinner

---
+++ app/src/main/java/com/proshiftteam/proshift/DataFiles/EmployeeObject.kt
@@ -0,0 +1,8 @@
+package com.proshiftteam.proshift.DataFiles
+
+data class EmployeeObject (
+    var id: Int = 0,
+    var employee_name: String = "",
+    var position: Int = 0,
+    var position_name: String = ""
+)
