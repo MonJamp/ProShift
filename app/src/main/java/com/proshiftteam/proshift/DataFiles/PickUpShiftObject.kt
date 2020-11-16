aae65481a2b87300db6edbe931b1c6b71d6a01b5
Rupin Patel <70920375+rupinpatel@users.noreply.github.com>
2020-11-15 20:19:38 -0600

    Functional pick up shift button
    Added code in ApiCalls to make shift pick up button functional.
    Added on click listener for pickup shift image in the open shifts menu.
    Created a pickup shift object.

---
+++ app/src/main/java/com/proshiftteam/proshift/DataFiles/PickUpShiftObject.kt
@@ -0,0 +1,8 @@
+package com.proshiftteam.proshift.DataFiles
+
+data class PickUpShiftObject (
+    var company: Int = 0,
+    var employee: Int = 0,
+    var shift: Int = 0,
+    var is_approved: Boolean = true
+)
