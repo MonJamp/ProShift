6311ab5fe8e922c2a29e3084b4eff17798065c37
Rupin Patel <70920375+rupinpatel@users.noreply.github.com>
2020-11-18 20:52:54 -0600

    Update GetTimeOffRequestsObject.kt

--- app/src/main/java/com/proshiftteam/proshift/DataFiles/GetTimeOffRequestsObject.kt
+++ app/src/main/java/com/proshiftteam/proshift/DataFiles/GetTimeOffRequestsObject.kt
@@ -4,9 +4,9 @@ data class GetTimeOffRequestsObject (
     var id: Int = 0,
     var company: Int = 0,
     var employee_name: String = "",
-    var is_approved: Boolean = false,
-    var start_date: String = "",
-    var end_date: String = "",
-    var time_start: String = "",
-    var time_end: String = ""
+    var is_approved: Boolean,
+    var start_date: String,
+    var end_date: String,
+    var time_start: String,
+    var time_end: String
 )
