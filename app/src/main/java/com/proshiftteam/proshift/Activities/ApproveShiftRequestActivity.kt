0d036466c53bac90b9123c9702d00641af3049b2
Rupin Patel <70920375+rupinpatel@users.noreply.github.com>
2020-11-19 00:05:22 -0600

    Update ApproveShiftRequestActivity.kt
    Added code to call retrofit object to show pending shift requests

--- app/src/main/java/com/proshiftteam/proshift/Activities/ApproveShiftRequestActivity.kt
+++ app/src/main/java/com/proshiftteam/proshift/Activities/ApproveShiftRequestActivity.kt
@@ -3,8 +3,16 @@ package com.proshiftteam.proshift.Activities
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.proshiftteam.proshift.Adapters.GetShiftRequestsAdapter
+import com.proshiftteam.proshift.DataFiles.GetShiftRequestsObject
+import com.proshiftteam.proshift.Interfaces.RetrofitBuilderObject
 import com.proshiftteam.proshift.R
+import kotlinx.android.synthetic.main.activity_approve_shift_requests.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ApproveShiftRequestActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +24,26 @@ class ApproveShiftRequestActivity: AppCompatActivity() {
         val bundle: Bundle? = intent.extras
         val tokenCode: String? = bundle?.getString("tokenCode")
 
+        val callApiGetShiftRequests: Call<List<GetShiftRequestsObject>> = RetrofitBuilderObject.connectJsonApiCalls.getShiftRequests("Token $tokenCode")
+        callApiGetShiftRequests.enqueue(object: Callback<List<GetShiftRequestsObject>> {
+            override fun onFailure(call: Call<List<GetShiftRequestsObject>>, t: Throwable) {
+                Toast.makeText(context, "Cannot connect! Error displaying shift requests!", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onResponse(
+                call: Call<List<GetShiftRequestsObject>>,
+                response: Response<List<GetShiftRequestsObject>>
+            ) {
+                if (response.isSuccessful) {
+                    Toast.makeText(context, "Successfully loaded shift requests" + response.code(), Toast.LENGTH_SHORT).show()
+                    val listOfShiftRequests = response.body()!!
+                    approve_shift_requests_recycler_view.adapter = GetShiftRequestsAdapter(tokenCode.toString(), listOfShiftRequests)
+                } else {
+                    Toast.makeText(context, "Failed loading shift requests : Response code " + response.code(), Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        })
         findViewById<ImageView>(R.id.backArrowButtonApproveShiftRequests).setOnClickListener {
             val intentToManagerControlsActivity = Intent(context, ManagerControlsActivity::class.java)
             intentToManagerControlsActivity.putExtra("tokenCode", tokenCode)
