de6c176144041affd613d5e80c564953d57ad843
Rupin Patel <70920375+rupinpatel@users.noreply.github.com>
2020-11-21 18:31:02 -0600

    Modify Shifts (shifts list)
    Changed the button names in manager controls.
    Created a ViewAllShifts object
    Added API call for view all shifts for managers
    Created a view all shifts adapter
    Added retrofit builder in modify shifts activivty
    Created a card item for the shifts

--- app/src/main/java/com/proshiftteam/proshift/Activities/CurrentShiftAddRemoveActivity.kt
+++ app/src/main/java/com/proshiftteam/proshift/Activities/CurrentShiftAddRemoveActivity.kt
@@ -3,13 +3,22 @@ package com.proshiftteam.proshift.Activities
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.proshiftteam.proshift.Adapters.MyAdapter
+import com.proshiftteam.proshift.Adapters.ViewAllShiftsAdapter
 import com.proshiftteam.proshift.DataFiles.HMS
 import com.proshiftteam.proshift.DataFiles.ScheduleData
+import com.proshiftteam.proshift.DataFiles.ViewAllShiftsObject
+import com.proshiftteam.proshift.Interfaces.RetrofitBuilderObject
 import com.proshiftteam.proshift.R
+import kotlinx.android.synthetic.main.activity_current_shift_add_remove.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
 
 class CurrentShiftAddRemoveActivity: AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
@@ -23,77 +32,31 @@ class CurrentShiftAddRemoveActivity: AppCompatActivity() {
         val bundle: Bundle? = intent.extras
         val tokenCode: String? = bundle?.getString("tokenCode")
 
+        val callApiViewAllShifts: Call<List<ViewAllShiftsObject>> = RetrofitBuilderObject.connectJsonApiCalls.viewAllShifts("Token $tokenCode")
+        callApiViewAllShifts.enqueue(object: Callback<List<ViewAllShiftsObject>> {
+            override fun onFailure(call: Call<List<ViewAllShiftsObject>>, t: Throwable) {
+                Toast.makeText(context, "Cannot connect! Error displaying all the shifts!", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onResponse(
+                call: Call<List<ViewAllShiftsObject>>,
+                response: Response<List<ViewAllShiftsObject>>
+            ) {
+                if (response.isSuccessful) {
+                    Toast.makeText(context, "Successfully displaying all the shifts " + response.code(), Toast.LENGTH_SHORT).show()
+                    val listOfEveryShift = response.body()!!
+                    recycler_view_View_all_shifts.adapter = ViewAllShiftsAdapter(tokenCode.toString(), listOfEveryShift)
+                } else {
+                    Toast.makeText(context, "Failed loading open shifts : Response code " + response.code(), Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        })
+
         findViewById<ImageView>(R.id.backArrowButtonAddRemove).setOnClickListener {
             val intentToManagerControlsActivity = Intent(context, ManagerControlsActivity::class.java)
             intentToManagerControlsActivity.putExtra("tokenCode", tokenCode)
             startActivity(intentToManagerControlsActivity)
-        }
-
-
-        viewManager = LinearLayoutManager(this)
-        val exampleData: Array<ScheduleData> = arrayOf(
-            ScheduleData(
-                "September",
-                21,
-                HMS(9, 0),
-                HMS(5, 0)
-            ),
-            ScheduleData(
-                "November",
-                22,
-                HMS(10, 0),
-                HMS(5, 0)
-            ),
-            ScheduleData(
-                "October",
-                25,
-                HMS(12, 0),
-                HMS(5, 0)
-            ),
-
-            // Added more values for testing purposes
-            ScheduleData(
-                "September",
-                21,
-                HMS(9, 0),
-                HMS(5, 0)
-            ),
-            ScheduleData(
-                "November",
-                22,
-                HMS(10, 0),
-                HMS(5, 0)
-            ),
-            ScheduleData(
-                "October",
-                25,
-                HMS(12, 0),
-                HMS(5, 0)
-            ),
-            ScheduleData(
-                "September",
-                21,
-                HMS(9, 0),
-                HMS(5, 0)
-            ),
-            ScheduleData(
-                "November",
-                22,
-                HMS(10, 0),
-                HMS(5, 0)
-            ),
-            ScheduleData(
-                "October",
-                25,
-                HMS(12, 0),
-                HMS(5, 0)
-            )
-        )
-        viewAdapter = MyAdapter(exampleData)
-        recyclerView = findViewById<RecyclerView>(R.id.recyclerViewSampleList).apply {
-            setHasFixedSize(true)
-            layoutManager = viewManager
-            adapter = viewAdapter
         }
 
     }
