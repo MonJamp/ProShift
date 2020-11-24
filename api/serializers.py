3448e38c562fe0bef14564cb440b8cd0bb0e4798
MonJamp <filjoabraham@gmail.com>
2020-11-23 21:58:42 -0600

    Remove unused imports

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,5 @@
-from djoser.serializers import UserCreateSerializer, UserSerializer
+from djoser.serializers import UserCreateSerializer
 from rest_framework import serializers
-from rest_framework.fields import ReadOnlyField
-from rest_framework.validators import UniqueValidator
 
 from dashboard.models import *
 
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.base_user import BaseUserManager
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from smart_selects.db_fields import ChainedForeignKey
