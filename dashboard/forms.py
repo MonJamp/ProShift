3765fc95763603998b9edbbe35c6cbaeeee5983e
MonJamp <filjoabraham@gmail.com>
2020-11-23 21:52:58 -0600

    Remove company code field from user model

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -6,7 +6,7 @@ from .models import User, Shift
 class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'phone', 'company_code', 'password1', 'password2']
+        fields = ['username', 'email', 'first_name', 'last_name', 'phone', 'password1', 'password2']
 
 class NewShiftForm(ModelForm):
     class Meta:
