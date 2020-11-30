9516ab0dae520e4c10706e0fe22cbbbecee26c19
Rupin Patel <70920375+rupinpatel@users.noreply.github.com>
2020-11-30 16:08:29 -0600

    Pending shifts modification
    Created a pending shift requests Object
    Created a pending shift adapter.
    Added API call in apicalls.kt
    added code into android manifest.

--- app/src/main/java/com/proshiftteam/proshift/Activities/PendingShiftRequestActivity.kt
+++ app/src/main/java/com/proshiftteam/proshift/Activities/PendingShiftRequestActivity.kt
@@ -3,8 +3,16 @@ package com.proshiftteam.proshift.Activities
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.proshiftteam.proshift.Adapters.PendingShiftsAdapter
+import com.proshiftteam.proshift.DataFiles.PendingShiftRequestsObject
+import com.proshiftteam.proshift.Interfaces.RetrofitBuilderObject
 import com.proshiftteam.proshift.R
+import kotlinx.android.synthetic.main.activity_pending_shift_requests.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class PendingShiftRequestActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +25,23 @@ class PendingShiftRequestActivity: AppCompatActivity() {
         val tokenCode: String? = bundle?.getString("tokenCode")
         val accessCode: Int? = bundle?.getInt("accessCode")
 
+        val callApiShowPendingShifts: Call<List<PendingShiftRequestsObject>> = RetrofitBuilderObject.connectJsonApiCalls.showPendingShiftRequests("Token $tokenCode")
+        callApiShowPendingShifts.enqueue(object: Callback<List<PendingShiftRequestsObject>> {
+            override fun onFailure(call: Call<List<PendingShiftRequestsObject>>, t: Throwable) {
+                Toast.makeText(context, "Cannot connect! Error displaying pending shifts!", Toast.LENGTH_SHORT).show()
+            }
 
+            override fun onResponse(call: Call<List<PendingShiftRequestsObject>>, response: Response<List<PendingShiftRequestsObject>>) {
+                if (response.isSuccessful) {
+                    Toast.makeText(context, "Successfully loaded pending shifts " + response.code(), Toast.LENGTH_SHORT).show()
+                    val listOfPendingShifts = response.body()!!
+                    pendingShiftRequestsRecyclerView.adapter = PendingShiftsAdapter(tokenCode.toString(), listOfPendingShifts)
+                } else {
+                    Toast.makeText(context, "Failed loading open shifts : Response code " + response.code(), Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        })
         findViewById<ImageView>(R.id.backArrowButtonPendingShiftRequests).setOnClickListener {
             val intentToHomeActivity = Intent(context, HomeActivity::class.java)
             intentToHomeActivity.putExtra("tokenCode", tokenCode)
